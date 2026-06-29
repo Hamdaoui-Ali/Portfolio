da86eaf786d82fb35225f6cf0f57155500df0985
Ali <alihamdaoui000@gmail.com>
2026-06-29 01:32:40 +0100

    Implemented milestone F - issue HAM-167

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
-
+import sitemap from "@astrojs/sitemap"
 import robotsTxt from "astro-robots-txt"
 import { SITE_URL } from "./src/data/site"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [tailwind(), sitemap(), robotsTxt()],
   site: SITE_URL,
 })
